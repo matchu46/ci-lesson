aa3f204441e11332cac9b43a2fcb3afd5824b781
Matthew <mattedelson46@gmail.com>
2024-02-07 21:06:52 -0700

    commited some changes

---
+++ tests/test_script.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+def test_script_output(capfd):
+    
+    #run the script file under a new process using the same python interpretor
+    subprocess.run([sys.executable,'./script.py'])
+    
+    #capture the process output
+    captured = capfd.readouterr()
+    
+    #assert that the output is correct
+    assert captured.out == "Hello World!\r\n"
