3d09cd74a42bca9efdf7d1b0eaa40868a6e2c26f
matchu46 <123615321+matchu46@users.noreply.github.com>
2024-02-07 21:24:38 -0700

    Update test_script.py

--- tests/test_script.py
+++ tests/test_script.py
@@ -3,7 +3,7 @@ import sys
 def test_script_output(capfd):
     
     #run the script file under a new process using the same python interpretor
-    subprocess.run([sys.executable,'./script.py'])
+    subprocess.run([sys.executable,'../script.py'])
     
     #capture the process output
     captured = capfd.readouterr()
